66d9d892b5e06f414be3c55aa4f69f12ff552269
lazyf1sh <f1sh@pm.me>
2016-07-17 17:58:29 +0300

    initial

---
+++ entry.js
@@ -0,0 +1,3 @@
+var script = document.createElement('script');
+script.src = "https://raw.githubusercontent.com/kopylov-ivan/greasemonkey-github-hosting-boilerplate/master/main.js"
+document.body.appendChild(script);
