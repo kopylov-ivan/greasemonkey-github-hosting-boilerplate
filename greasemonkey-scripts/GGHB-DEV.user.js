2a10ba94eee26b93023c9f7b9c9d36fafd29464e
lazyf1sh <f1sh@pm.me>
2016-07-17 18:01:19 +0300

    added greasemonkey scripts

---
+++ greasemonkey-scripts/GGHB-DEV.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        TEMP_DEV
+// @namespace   greasemonkey-github-hosting-boilerplate
+// @include     http://example.com/
+// @require     file:///C:/LOCAL_REPO/main.js
+// @grand 		none
+// ==/UserScript==
+
---
+++ greasemonkey-scripts/GGHB-USER.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        TEMP_USER
+// @namespace   greasemonkey
+// @include     http://example.com/
+// @require     https://raw.githubusercontent.com/kopylov-ivan/greasemonkey-github-hosting-boilerplate/master/entry.js
+// @grand 		none
+// ==/UserScript==
