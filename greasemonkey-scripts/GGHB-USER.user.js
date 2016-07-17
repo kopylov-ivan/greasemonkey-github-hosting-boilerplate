6b57e3c019e67f7a3786c3d5999159ac64f83517
lazyf1sh <f1sh@pm.me>
2016-07-17 18:04:35 +0300

    Update GGHB-USER.user.js

--- greasemonkey-scripts/GGHB-USER.user.js
+++ greasemonkey-scripts/GGHB-USER.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        TEMP_USER
-// @namespace   greasemonkey
+// @namespace   greasemonkey-github-hosting-boilerplate
 // @include     http://example.com/
 // @require     https://raw.githubusercontent.com/kopylov-ivan/greasemonkey-github-hosting-boilerplate/master/entry.js
 // @grand 		none
